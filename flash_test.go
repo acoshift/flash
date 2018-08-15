0a560beba30ccd7e9b35b5d6da53849fa2d6e67c
acoshift <acoshift@gmail.com>
2018-08-15 15:57:08 +0700

    change test to bdd

--- flash_test.go
+++ flash_test.go
@@ -2,268 +2,420 @@ package flash
 
 import (
 	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestNew(t *testing.T) {
-	f := New()
-	if f == nil {
-		t.Errorf("expected New returns valid flash; got nil")
-	}
-	if f.Count() > 0 {
-		t.Errorf("expected New returns empty flash; got %v", f)
-	}
-	if f.Changed() {
-		t.Errorf("expected New returns unchanged flash; got changed")
-	}
+	Convey("Given new flash", t, func() {
+		f := New()
+
+		Convey("Flash should not be nil", func() {
+			So(f, ShouldNotBeNil)
+		})
+
+		Convey("Flash data should be empty", func() {
+			So(f.v, ShouldBeEmpty)
+		})
+
+		Convey("Flash count should be empty", func() {
+			So(f.Count(), ShouldEqual, 0)
+		})
+
+		Convey("Flash should not in changed state", func() {
+			So(f.Changed(), ShouldBeFalse)
+		})
+
+		Convey("Flash should be able to encode", func() {
+			b, err := f.Encode()
+
+			Convey("Without error", func() {
+				So(err, ShouldBeNil)
+			})
+
+			Convey("Encoded result should not be nil", func() {
+				So(b, ShouldNotBeNil)
+			})
+
+			Convey("Encoded bytes should be empty", func() {
+				So(b, ShouldBeEmpty)
+			})
+
+			Convey("Flash still unchanged", func() {
+				So(f.Changed(), ShouldBeFalse)
+			})
+		})
+
+		Convey("When add a data to flash", func() {
+			f.Add("a", 1)
+
+			Convey("Flash should be in changed state", func() {
+				So(f.Changed(), ShouldBeTrue)
+			})
+
+			Convey("Count should changed", func() {
+				So(f.Count(), ShouldEqual, 1)
+			})
+		})
+	})
+}
+
+func TestClear(t *testing.T) {
+	Convey("Given empty flash", t, func() {
+		f := New()
+
+		Convey("When clear", func() {
+			f.Clear()
+
+			Convey("Should not be in changed state", func() {
+				So(f.Changed(), ShouldBeFalse)
+			})
+		})
+	})
+
+	Convey("Given not empty flash", t, func() {
+		f := New()
+		f.Add("a", 1)
+
+		Convey("When clear", func() {
+			f.Clear()
+
+			Convey("Flash data should be empty", func() {
+				So(f.v, ShouldBeEmpty)
+			})
+
+			Convey("Count should be zero", func() {
+				So(f.Count(), ShouldBeZeroValue)
+			})
+
+			Convey("Should be in changed state", func() {
+				So(f.Changed(), ShouldBeTrue)
+			})
+
+			Convey("When clear again", func() {
+				Convey("Should still be in changed state", func() {
+					So(f.Changed(), ShouldBeTrue)
+				})
+			})
+		})
+	})
 }
 
 func TestEncodeDecode(t *testing.T) {
-	var (
-		f, p *Flash
-		b    []byte
-		err  error
-	)
+	Convey("Given not empty flash", t, func() {
+		f := New()
+		f.Add("a", 1)
 
-	f = New()
-	b, err = f.Encode()
-	if err != nil {
-		t.Errorf("expected Encode empty flash not error; got %v", err)
-	}
-	if len(b) > 0 {
-		t.Errorf("expected Encode empty flash returns zero length bytes; got %v", b)
-	}
-	if f.Changed() {
-		t.Errorf("expected Encode empty flash still unchange; got changed")
-	}
-	p, err = Decode([]byte{})
-	if err != nil {
-		t.Errorf("expected Decode empty bytes not error; got %v", err)
-	}
-	if p == nil {
-		t.Errorf("expected Decode empty bytes returns non-nil flash; got nil")
-	}
-	if p.Count() > 0 {
-		t.Errorf("expected Decode empty bytes returns empty flash; got %v", nil)
-	}
-	if f.Changed() {
-		t.Errorf("expected Decode empty flash returns unchange flash; got changed")
-	}
+		Convey("Should be able to encode", func() {
+			b, err := f.Encode()
 
-	f = New()
-	f.Add("a", "1")
-	if !f.Changed() {
-		t.Errorf("expected Add data to empty flash must changed; got unchange")
-	}
-	b, err = f.Encode()
-	if err != nil {
-		t.Errorf("expected Encode non-zero length flash not error; got %v", err)
-	}
-	if len(b) == 0 {
-		t.Errorf("expected Encode non-zero length flash returns non-zero length bytes; got %v", b)
-	}
-	if !f.Changed() {
-		t.Errorf("expected Encode changed flash still changed; got unchange")
-	}
-	p, err = Decode(b)
-	if err != nil {
-		t.Errorf("expected Decode not error; got %v", err)
-	}
-	if n := f.Count(); n != 1 {
-		t.Errorf("expected Decode returns 1 key flash; got %d", n)
-	}
-	if p.Changed() {
-		t.Errorf("expected Decode not empty flash must unchange; got changed")
-	}
-	p.Clear()
-	if !p.Changed() {
-		t.Errorf("expected clear not empty unchanged flash must changed; got unchange")
-	}
+			Convey("Without error", func() {
+				So(err, ShouldBeNil)
+			})
 
-	p, _ = Decode(b)
-	p.Del("a")
-	if !p.Changed() {
-		t.Errorf("expected Del from not empty unchanged flash must change; got unchanged")
-	}
+			Convey("Encoded result should not be nil", func() {
+				So(b, ShouldNotBeNil)
+			})
+
+			Convey("Encoded bytes should not be empty", func() {
+				So(b, ShouldNotBeEmpty)
+			})
+
+			Convey("Flash should still be changed state", func() {
+				So(f.Changed(), ShouldBeTrue)
+			})
+
+			Convey("Encoded data should be able to decode", func() {
+				f, err := Decode(b)
+
+				Convey("Without error", func() {
+					So(err, ShouldBeNil)
+				})
+
+				Convey("Decoded flash should not be nil", func() {
+					So(f, ShouldNotBeNil)
+				})
+
+				Convey("With same count", func() {
+					So(f.Count(), ShouldEqual, 1)
+				})
+
+				Convey("With same data", func() {
+					So(f.v["a"], ShouldNotBeEmpty)
+					So(f.v["a"][0], ShouldEqual, 1)
+				})
+			})
+		})
+	})
+
+	Convey("Given flash with invalid data", t, func() {
+		f := New()
+		f.Set("key", &struct{}{})
+
+		Convey("When encode", func() {
+			b, err := f.Encode()
+
+			Convey("Should error", func() {
+				So(err, ShouldNotBeNil)
+			})
+
+			Convey("Encoded bytes should be empty", func() {
+				So(b, ShouldBeEmpty)
+			})
+		})
+	})
+
+	Convey("Given empty bytes", t, func() {
+		b := []byte{}
+
+		Convey("Bytes should be able to decode", func() {
+			f, err := Decode(b)
+
+			Convey("Without error", func() {
+				So(err, ShouldBeNil)
+			})
+
+			Convey("Flash should not be nil", func() {
+				So(f, ShouldNotBeNil)
+			})
+
+			Convey("Flash data should be empty", func() {
+				So(f.v, ShouldBeEmpty)
+			})
+
+			Convey("Flash count should be zero", func() {
+				So(f.Count(), ShouldBeZeroValue)
+			})
+
+			Convey("Flash should not in changed state", func() {
+				So(f.Changed(), ShouldBeFalse)
+			})
+		})
+	})
+
+	Convey("Given invalid encoded flash bytes", t, func() {
+		b := []byte("invalid data")
+
+		Convey("When decode", func() {
+			f, err := Decode(b)
+
+			Convey("Should error", func() {
+				So(err, ShouldNotBeNil)
+			})
+
+			Convey("Returns flash should be nil", func() {
+				So(f, ShouldBeNil)
+			})
+		})
+	})
 }
 
-func TestOperators(t *testing.T) {
-	f := New()
+func TestDel(t *testing.T) {
+	Convey("Given not empty flash", t, func() {
+		f := New()
+		f.Add("a", 1)
 
-	if f.Has("a") {
-		t.Errorf("expected has empty flash return false; got true")
-	}
-	if f.Get("a") != nil {
-		t.Errorf("expected get from empty flash return nil")
-	}
-	l := f.Values("a")
-	if l == nil {
-		t.Errorf("expected value always return non-nil slice")
-	}
-	if len(l) > 0 {
-		t.Errorf("expected value from empty flash return zero-length slice")
-	}
+		Convey("When delete all keys", func() {
+			f.Del("a")
 
-	f.Set("a", "1")
-	f.Set("b", "2")
-	f.Add("c", "3")
-	f.Add("c", "4")
-
-	if !f.Has("a") {
-		t.Errorf("expected f has 'a'")
-	}
-	if !f.Has("c") {
-		t.Errorf("expected f has 'c'")
-	}
-	if p := f.Get("a"); p != "1" {
-		t.Errorf("expected Get 'a' from f is 1; got %s", p)
-	}
-	if p := f.Get("c"); p != "3" {
-		t.Errorf("expected Get 'c' from f is 3; got %s", p)
-	}
-	if f.Has("c") {
-		t.Errorf("expected 'c' remove from f after get")
-	}
-
-	f.Del("b")
-	if f.Has("b") {
-		t.Errorf("expected f don't have b")
-	}
-
-	f.Del("empty")
-	if f.Has("empty") {
-		t.Errorf("expected f don't have empty")
-	}
-
-	f.Add("c", "3")
-	f.Add("c", "4")
-
-	l = f.Values("c")
-	if len(l) != 2 {
-		t.Errorf("expected value from 'c' return 2 length slice; got %d", len(l))
-	}
-	if l[0].(string) != "3" && l[1].(string) != "4" {
-		t.Errorf("expected value return values from added")
-	}
-
-	f.Clear()
-	if f.Count() > 0 {
-		t.Errorf("expected f empty after clear")
-	}
-
-	f.Set("string", "string value")
-	if f.GetString("string") != "string value" {
-		t.Errorf("expected get string return valid value")
-	}
-	f.Set("int", 1)
-	if f.GetInt("int") != 1 {
-		t.Errorf("expected get int return valid value")
-	}
-	f.Set("int64", int64(1))
-	if f.GetInt64("int64") != int64(1) {
-		t.Errorf("expected get int64 return valid value")
-	}
-	f.Set("float32", float32(1.2))
-	if f.GetFloat32("float32") != float32(1.2) {
-		t.Errorf("expected get float32 return valid value")
-	}
-	f.Set("float64", float64(1.5))
-	if f.GetFloat64("float64") != float64(1.5) {
-		t.Errorf("expected get float64 return valid value")
-	}
-	f.Set("bool", true)
-	if f.GetBool("bool") != true {
-		t.Errorf("expected get bool return valid value")
-	}
-
-	if len(f.Values("empty")) != 0 {
-		t.Errorf("expected value from empty key return zero-length slice")
-	}
-
-	f.v["a"] = []interface{}{}
-	if f.Get("a") != nil {
-		t.Errorf("expected get from non-nil, zero-length key return nil")
-	}
+			Convey("Should still be in changed state", func() {
+				So(f.Changed(), ShouldBeTrue)
+			})
+		})
+	})
 }
 
-func equals(f *Flash, p *Flash) bool {
-	if f.Count() != p.Count() {
-		return false
-	}
-	for k := range f.v {
-		if len(f.v[k]) != len(p.v[k]) {
-			return false
-		}
-		for kk := range f.v[k] {
-			if f.v[k][kk] != p.v[k][kk] {
-				return false
-			}
-		}
-	}
-	return true
+func TestValues(t *testing.T) {
+	Convey("Given empty flash", t, func() {
+		f := New()
+
+		Convey("When retrieve values from not exists key", func() {
+			v := f.Values("a")
+
+			Convey("Should return non-nil values", func() {
+				So(v, ShouldNotBeNil)
+			})
+
+			Convey("Should return empty data", func() {
+				So(v, ShouldBeEmpty)
+			})
+		})
+
+		Convey("When add 3 values same key", func() {
+			f.Add("a", 1)
+			f.Add("a", 2)
+			f.Add("a", 3)
+
+			Convey("When retrieve values for that key", func() {
+				v := f.Values("a")
+
+				Convey("Should resemble added values", func() {
+					So(v, ShouldResemble, []interface{}{1, 2, 3})
+				})
+
+				Convey("Should not disapper in flash", func() {
+					So(f.Has("a"), ShouldBeFalse)
+				})
+			})
+		})
+	})
+}
+
+func TestGet(t *testing.T) {
+	Convey("Given empty flash", t, func() {
+		f := New()
+
+		Convey("When get not exists key", func() {
+			v := f.Get("a")
+
+			Convey("Should return nil", func() {
+				So(v, ShouldBeNil)
+			})
+
+			Convey("Should still be unchanged state", func() {
+				So(f.Changed(), ShouldBeFalse)
+			})
+		})
+
+		Convey("When set a value", func() {
+			f.Set("a", 1)
+
+			Convey("Should has that key", func() {
+				So(f.Has("a"), ShouldBeTrue)
+			})
+
+			Convey("When get that key", func() {
+				v := f.Get("a")
+
+				Convey("Should return same value", func() {
+					So(v, ShouldEqual, 1)
+				})
+
+				Convey("Key should disappear", func() {
+					So(f.Has("a"), ShouldBeFalse)
+				})
+			})
+		})
+
+		Convey("When set a string value", func() {
+			f.Set("a", "hello")
+
+			Convey("When get string from that key", func() {
+				v := f.GetString("a")
+
+				Convey("Should return same value", func() {
+					So(v, ShouldEqual, "hello")
+				})
+			})
+
+			Convey("When get int from that key", func() {
+				v := f.GetInt("a")
+
+				Convey("Should return zero value", func() {
+					So(v, ShouldBeZeroValue)
+				})
+			})
+
+			Convey("When get int64 from that key", func() {
+				v := f.GetInt64("a")
+
+				Convey("Should return zero value", func() {
+					So(v, ShouldBeZeroValue)
+				})
+			})
+
+			Convey("When get float32 from that key", func() {
+				v := f.GetFloat32("a")
+
+				Convey("Should return zero value", func() {
+					So(v, ShouldBeZeroValue)
+				})
+			})
+
+			Convey("When get float64 from that key", func() {
+				v := f.GetFloat64("a")
+
+				Convey("Should return zero value", func() {
+					So(v, ShouldBeZeroValue)
+				})
+			})
+
+			Convey("When get bool from that key", func() {
+				v := f.GetBool("a")
+
+				Convey("Should return zero value", func() {
+					So(v, ShouldBeZeroValue)
+				})
+			})
+		})
+	})
 }
 
 func TestClone(t *testing.T) {
-	f := New()
-	f.Add("a", "1")
-	f.Add("a", "2")
-	f.Add("b", "3")
+	Convey("Given not empty flash", t, func() {
+		f := New()
+		f.Add("a", "1")
+		f.Add("a", "2")
+		f.Add("b", "3")
 
-	p := f.Clone()
+		Convey("When clone", func() {
+			p := f.Clone()
 
-	if f == p {
-		t.Errorf("expected cloned flash don't have same pointer")
-	}
+			Convey("Should not point to original", func() {
+				So(p, ShouldNotPointTo, f)
+			})
 
-	if !equals(f, p) {
-		t.Errorf("expected cloned encode to be same value")
-	}
+			Convey("Data should have same count", func() {
+				So(p.Count(), ShouldEqual, f.Count())
+			})
 
-	f.Clear()
-	if equals(f, p) {
-		t.Errorf("expected cloned encode and cleard original not same value")
-	}
-}
+			Convey("Data should have same values", func() {
+				So(p.v, ShouldResemble, f.v)
+			})
 
-func TestEncodeError(t *testing.T) {
-	v := struct{}{}
-	f := New()
-	f.Set("key", &v)
-	b, err := f.Encode()
-	if err == nil {
-		t.Errorf("expected encode unregistered gob struct error; got nil")
-	}
-	if b == nil {
-		t.Errorf("expected result of encode error not nil")
-	}
-	if len(b) > 0 {
-		t.Errorf("expected length of encode error to be 0; got nil")
-	}
-}
+			Convey("When clear original flash", func() {
+				f.Clear()
 
-func TestDecodeError(t *testing.T) {
-	f, err := Decode([]byte("invalid data"))
-	if err == nil {
-		t.Errorf("expected decode invalid data returns error; got nil")
-	}
-	if f != nil {
-		t.Errorf("expected decode invalid data returns nil flash; got %v", f)
-	}
-}
+				Convey("Clone data should not resemble original data", func() {
+					So(p.v, ShouldNotResemble, f.v)
+				})
+			})
+		})
 
-func TestClearEmpty(t *testing.T) {
-	f := New()
-	f.Clear()
-	if f.Changed() {
-		t.Errorf("expected clear empty flash must not changed")
-	}
+	})
 }
 
 func TestCount(t *testing.T) {
-	f := New()
-	f.Set("a", true)
-	f.Get("a")
+	Convey("Given empty flash", t, func() {
+		f := New()
 
-	if f.Count() != 0 {
-		t.Errorf("expected count return 0 when already got value")
-	}
+		Convey("Should have 0 count", func() {
+			So(f.Count(), ShouldEqual, 0)
+		})
+
+		Convey("When set a value", func() {
+			f.Set("a", true)
+
+			Convey("Should have 1 count", func() {
+				So(f.Count(), ShouldEqual, 1)
+			})
+
+			Convey("When get that value", func() {
+				f.Get("a")
+
+				Convey("Should back to 0 count", func() {
+					So(f.Count(), ShouldEqual, 0)
+				})
+			})
+		})
+
+		Convey("When set 2 values", func() {
+			f.Set("a", 1)
+			f.Set("b", 2)
+
+			Convey("Should have 2 count", func() {
+				So(f.Count(), ShouldEqual, 2)
+			})
+		})
+	})
 }
